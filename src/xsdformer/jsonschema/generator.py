2cbfb176556cbcf4d33594587dc7075a9c601cfb
Tobias Sargeant <toby.sargeant@populationgenomics.org.au>
2025-10-20 11:28:25 +1100

    address gemini comments

--- src/xsdformer/jsonschema/generator.py
+++ src/xsdformer/jsonschema/generator.py
@@ -149,10 +149,9 @@ class _JsonSchemaFromDesc:
 
         if file_name in self._source_info and tuple(path) in self._source_info[file_name]:
             loc = self._source_info[file_name][tuple(path)]
-            if loc.leading_comments:
-                return loc.leading_comments.strip()
-            if loc.trailing_comments:
-                return loc.trailing_comments.strip()
+            comment = loc.leading_comments or loc.trailing_comments
+            if comment:
+                return comment.strip()
         return None
 
     def _get_message_path(self, desc: descriptor.Descriptor) -> list[int]:
